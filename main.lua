68497750a261af15d57437a5d51a5c3fc1adac51
Nicholas <nicholasaxel@protonmail.com>
2021-02-22 21:32:54 +0700

    proto type of player movement

--- main.lua
+++ main.lua
@@ -20,7 +20,9 @@ function love.load()
     playerx = VIRTUAL_WIDTH / 2 
     playerdy = 0
     playerdx = 0
-
+    gStateMachine = {
+        ['sta
+    }
     -- statemachine implementation
 
     love.keyboard.keypressed = {}
